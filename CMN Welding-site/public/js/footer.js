1fc79b1546c7652f182e06b7b19f7d273bd73139
Richard Block <richard.block@snhu.edu>
2026-03-11 20:34:55 -0500

    Update footer.js

--- CMN Welding-site/public/js/footer.js
+++ CMN Welding-site/public/js/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("../footer.html")
+   fetch("../components/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer-placeholder").innerHTML = data;
